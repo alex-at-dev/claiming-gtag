9c49bce196e7a392cb79be387762a000c330430f
alex <alex.zeiss@gmx.net>
2024-01-26 10:39:58 +0100

    Always send index.html no matter the requested route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import path from 'path';
 dotenv.config();
 
@@ -8,7 +8,33 @@ import { HOST, PORT } from './config';
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../../client/dist')));
+app.get('*', (_req, res) => res.sendFile(path.join(__dirname, '../../client/dist/index.html')));
+
+app.use(catch404);
+app.use(globalErrorHandler);
 
 app.listen({ host: HOST, port: PORT }, () => {
   console.log(`Listening on ${HOST}:${PORT}`);
 });
+
+// HANDLER
+function catch404(_req: Request, _res: Response, next: NextFunction) {
+  let msg = '404 - Not found';
+  let err = new Error(msg);
+  next(err);
+}
+
+function globalErrorHandler(err: any, req: Request, res: Response, _next: NextFunction) {
+  err.status = err.status || 500;
+  console.error(err);
+  res.status(err.status);
+  res.json({
+    req: {
+      url: req.url,
+    },
+    error: {
+      status: err.status,
+      message: err.message,
+    },
+  });
+}
