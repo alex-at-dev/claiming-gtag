3b199f49fa361f69b7304e84e2096ced2ab31dbd
alex <alex.zeiss@gmx.net>
2024-01-30 10:02:53 +0100

    Fix tracking events

--- client/src/util/gaTrackForms.ts
+++ client/src/util/gaTrackForms.ts
@@ -5,25 +5,25 @@ const gtag = (type: string, name: string, data: Record<string, string | number |
 
 const sendGtagInputEvent = (name: string, ev: Event) => {
   const target = ev.target as HTMLInputElement;
-  if (!target || target.tagName !== 'INPUT' || !target.dataset.gaid) return;
-  gtag('event', 'name', {
+  if (!target || target.tagName !== 'INPUT') return;
+  gtag('event', name, {
     event_category: 'engagement',
     event_label: name,
-    field_name: target.name,
+    field_name: target.dataset.gaid || target.id || target.name,
     field_is_filled: !!target.value,
   });
 };
 
-document.addEventListener('click', (ev) => sendGtagInputEvent('text_field_focus', ev));
-document.addEventListener('blur', (ev) => sendGtagInputEvent('text_field_blur', ev));
+document.addEventListener('focusin', (ev) => sendGtagInputEvent('text_field_focus', ev));
+document.addEventListener('focusout', (ev) => sendGtagInputEvent('text_field_blur', ev));
 
 document.addEventListener('click', (ev) => {
   const target = ev.target as HTMLButtonElement;
-  if (!target || target.tagName !== 'BUTTON' || !target.dataset.gaid) return;
+  if (!target || target.tagName !== 'BUTTON') return;
   gtag('event', 'button_click', {
     event_category: 'engagement',
     event_label: 'button_click',
-    field_name: target.dataset.gaid,
+    field_name: target.dataset.gaid || target.id || target.name,
   });
 });
 
